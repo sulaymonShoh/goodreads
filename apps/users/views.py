2ec3db900a41e08460fd85d3a04fca59d5a79c68
Sulaymonqori <ms13061312@gmail.com>
2024-02-16 15:09:58 +0500

    fix: login and register views added

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+from apps.users.forms import UserRegistrationForm, LoginForm
+
+
+class RegisterView(View):
+    def get(self, request):
+        form = UserRegistrationForm()
+        return render(request, 'auth/register.html', {'form': form})
+
+    def post(self, request):
+        form = UserRegistrationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            # login(request, user)
+            messages.success(request, "User Successfully created")
+            return redirect('blog:login')
+        return render(request, 'auth/register.html', {'form': form})
+
+
+class LoginView(View):
+    def get(self, request):
+        form = LoginForm()
+        return render(request, 'auth/login.html', {'form': form})
+
+    def post(self, request):
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Logged in successfully')
+                return redirect('blog:home_page')
+
+        messages.warning(request, 'Username or password wrong')
+        return render(request, 'auth/login.html', {'form': form})
+
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        messages.info(request, 'Logged out successfully')
+        return redirect('homepage')
